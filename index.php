06cdb4f1f35f2d2cd3c9f8d7d73b83b8708d1f27
Adarsh Bhardwaj <145377931+denock57@users.noreply.github.com>
2024-05-28 11:16:10 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,67 @@
 <?php 
     session_start();
-    if (!isset($_SESSION['email'])) {
-        header('location: demo.php');
-        exit();
-    }
+    include "server.php";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href=".css">
+    <title>CRUD</title>
+</head>
+<body>
+    <div class="container">
+        <div class="inserts">
+            <h1>Reservation Form</h1>
+            <form action="" method="post">
+                <input type="text" name="username"  placeholder=" Input Username " required=""><br>
+                <input type="email" name="email"  placeholder=" Input email "   required=""><br>
+                <input type="text" name="party"  placeholder=" Input Party Size "   required=""><br>
+                <input type="text" name="location"  placeholder=" Input Location "  required=""><br>
 
-<h1><?php echo $_SESSION['success'] . "  And You are User  " . $_SESSION['new_id']; ?> <br>
-<a href="demo.php?logout=<?php echo $_SESSION['new_id'] ?>">Logout</a>
-</h1>
+                <textarea name="detail"  placeholder="Add A discription" rows="10"  required="">
+                
+                </textarea><br>
+                <input type="submit" name="submit" value="SendNow">
+            </form>
+        </div>
+        <div class="views">
+        <h1>Administration View</h1>
+        <h2>
+            All Reservations Made Today <?php echo date('D, d M Y');?>
+        </h2>
+            <div class="links">
+                
+                    <?php 
+                     $query = "SELECT * FROM users";
+                     $return = mysqli_query($db, $query);
 
+                     if($return){
+                         if (mysqli_num_rows($return) > 0) {
+                             $new_reserv = mysqli_fetch_all($return, MYSQLI_ASSOC);
+
+                             foreach ($new_reserv as $value) {
+                            ?>
+                            <div class="body">
+                            <h4><?php echo $value['user'] ?></h4>
+                                <a href="view.php?detail=<?php echo $value['id'] ?>">
+                                    <?php 
+                                        if(strlen($value['detail']) >= 50) {
+                                            echo substr($value['detail'], 0, 50) . "...";
+                                            echo "<p class='view_more'>View More</p>";
+                                        } else {
+                                            echo $value['detail'];
+                                        }
+                                    ?>
+                                </a>
+                                </div>
+                             <?php } } } ?>
+                
+            </div>
+            <p style="color: white;"> <strong>NOTE:</strong> THIS SECTION WILL BE VISIBLE ONLY TO ADMINISTRATORS</p>
+        </div>
+    </div>
+</body>
+</html>
--- server.php
+++ server.php
@@ -1,50 +1,28 @@
-<?php
-    $db = mysqli_connect('localhost', 'root', '', 'demo1');
+<?php 
+// CREATE DATABASE CONNECTION
+ $db = mysqli_connect('localhost', 'root', '', 'reservation') or die("connection failed" . mysqli_error());    
+// SELECT FORM FIELD DATA
+ if(isset($_POST['submit'])){
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $party = mysqli_real_escape_string($db, $_POST['party']);
+    $location = mysqli_real_escape_string($db, $_POST['location']);
+    $detail = mysqli_real_escape_string($db, $_POST['detail']);
 
-// signup query
-   if (isset($_POST['submit'])) {
-       $username = mysqli_real_escape_string($db, $_POST['username']);
-       $email    = mysqli_real_escape_string($db, $_POST['email']);
-       $password1   = mysqli_real_escape_string($db, md5($_POST['password1']));
-       $password2   = mysqli_real_escape_string($db, md5($_POST['password2']));
- // authentication
-       if($password1 !== $password2) {
-          array_push($errors, 'Passwords do not match!');
-          header('location: demo.php');
-          exit();
-       }
-
-    //    VALIDATION
-       $check_query = "SELECT email FROM users WHERE email = '$email' "; 
-       $returns = mysqli_query($db, $check_query);
-          if (mysqli_num_rows($returns) > 0) {
-              array_push($errors, 'This User Email already exists');
-                  header('location: demo.php');
-                  exit();
-          } else {
-              $query = "INSERT INTO users(username, email, password) VALUES ('$username', '$email', '$password1') ";
-              $return = mysqli_query($db, $query);
-              if ($return) {
-                  $new_user = mysqli_fetch_all($return, MYSQLI_ASSOC);
-                  $_SESSION['new_id'] = $db->$insert_db;
-              }
-
-            }
-
-            $query = "SELECT * FROM users WHERE id = " . $_SESSION['new_id'];
-            $return_result = mysqli_query($db, $query);
-
-            if ($return_result) {
-                if ($mysqli_num_rows($return_result) > 0) {
-                    $new_user = mysqli_fetch_all($return_result, MYSQLI_ASSOC);
-                    $_SESSION['username'] = $new_user['username'];
-                    $_SESSION['email'] = $new_user['email'];
-                    $_SESSION['success'] = "Great!! You are now a member here! Your Email is: " . $_SESSION['email'];
-                    header('location: index.php');
-                    exit();
-                }
-            }
+    // QUERY
+    $query = mysqli_query($db, "INSERT INTO users(user,email,party,location,detail) VALUES('$username','$email','$party','$location','$detail') ");
+    if ($query) {
+        $_SESSION['success'] = "Your Reservation has been Submitted";
+        $_SESSION['id'] = $db->inser_id;
+        header('location: index.php');
+        exit();
+    } else {
+        $_SESSION['error'] = "Sorry, check your inputs for errors";
+    }
+ }
     
+
+//INSERT DATA TO DATABASE TO SAVE AS A RECORD
+ 
+
        
-}
-   
